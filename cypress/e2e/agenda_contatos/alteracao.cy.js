188918511499225d6a2e5bbfdee5ef00d1a48d15
fernanda <fernandatqfs@hotmail.com>
2025-03-21 14:56:10 -0300

    alteração final

--- cypress/e2e/agenda_contatos/alteracao.cy.js
+++ cypress/e2e/agenda_contatos/alteracao.cy.js
@@ -6,7 +6,10 @@ describe('Teste para alteração dos dados cadastrados', () =>{
 })
 
     it('Deve permitir editar as informações do contato e salvar após alterações', () => {
-        cy.get('.edit').first().click()
+         cy.get('.edit').first().click()
+         cy.get('input[type="text"]').clear().type('GIAN SOUZA')
+         cy.get('input[type="email"]').clear().type('Gian@gmail.com')
+         cy.get('input[type="tel"]').clear().type(11982212878)
         cy.get('.alterar').first().click()
 
 
